9a1d54c7a065c220373ee849e3cb5dab6dbd942f
Christopher Ray <vde435@gmail.com>
2024-01-12 10:13:43 -0600

    Create gamma_example.r

---
+++ gamma_example.r
@@ -0,0 +1,4 @@
+# Calculate the Gamma of a number
+gamma_value = gamma(5)  # This would compute Γ(5), which is equivalent to 4!
+print(gamma_value)
+print(factorial(4)) # for integers gamma(x) = factorial (x-1)
